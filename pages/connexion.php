78e230127e5ec950b466c1c3e28058ce2034e406
Gadiel <gadieldjibom961@gmail.com>
2024-05-16 11:17:47 +0000

    Mise en place de l'interface d'inscription

--- pages/connexion.php
+++ pages/connexion.php
@@ -3,13 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" vahref="./assets/css/style.css">
-    <title>Blo Adn</title>
+    <link rel="stylesheet" href="../assets/css/style.css"> 
+    <title>Connexion</title>
 </head>
 <body>
- <?php
- require_once './includes/menu.php';
-  ?>  
-
+    <?php
+    require_once'../includes/menu.php';
+     ?>
 </body>
 </html>
